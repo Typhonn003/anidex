664aba6e670710786047d035fbe6acabd53cf06e
Diego <diegolima.almeida93@gmail.com>
2024-02-28 13:03:56 -0300

    feat: Refactored functions to arrow functions, added dependencies: radix themes, axios, swiper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import "@radix-ui/themes/styles.css";
+
+import { Theme } from "@radix-ui/themes";
+
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Theme accentColor="plum">
+      <Component {...pageProps} />
+    </Theme>
+  );
+};
+
+export default App;
